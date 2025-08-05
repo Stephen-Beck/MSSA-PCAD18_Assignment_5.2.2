575eaca63b49fe54b7674ff4c5f022b73f140251
Stephen-Beck <stephenbeck89@outlook.com>
2025-08-05 16:57:04 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+Write a program in C# Sharp to print the first n natural number using recursion.
+
+    Test Data :
+        How many numbers to print : 10
+        Expected Output :
+            1 2 3 4 5 6 7 8 9 10
+*/
+
+namespace Assignment_5._2._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nums = 0;
+            while(true)
+            {
+                Console.Write("How many numbers would you like to print? ");
+                if (!int.TryParse(Console.ReadLine(), out nums) || nums < 1)
+                    Console.Write("Invalid number. ");
+                else break;
+            }
+            PrintNumber(nums);
+        }
+
+        static void PrintNumber(int n)
+        {
+            if (n > 1)
+                PrintNumber(n - 1);
+
+            Console.Write(n + " ");
+        }
+    }
+}
